2566f7af3d32be5a2894383e079179a290f3bdca
JackCaldwellNichols <jack.wcn@gmail.com>
2023-01-11 10:32:45 +0000

    api keys in .env, font applied to all pages, finished nearest cinema component

--- src/js/component/NearestCinemas.jsx
+++ src/js/component/NearestCinemas.jsx
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from "react";
 import '../../styles/home.css'
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
-import '../../styles/home.css'
+import '../../styles/nearest.css'
+
+const apiURL = process.env.GOOGLE_API
 
 const NearestCinema = () => {
 	const [currentLocation, setCurrentLocation] = useState({})
@@ -21,7 +23,7 @@ const NearestCinema = () => {
 	}, [])
 
 	const fetchPlace = async () => {
-		const URL = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyBHuHEnEY1Lnle97_UyKuOeDMHFxB1FR_Y&location=${currentLocation.latitude},${currentLocation.longitude}&radius=1000000&type=school`;
+		const URL = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${apiURL}&location=${currentLocation.latitude},${currentLocation.longitude}&radius=1000000&type=school`;
 		console.log(URL)
 		const CONFIG = {
 		  method: "GET",
@@ -63,20 +65,22 @@ const NearestCinema = () => {
 	return (
 		<div className="container" >
             	<h2 className="mb-4">Your nearest cinemas...</h2>
-			<div className="d-flex justify-content-around mx-2">
-				<AliceCarousel responsive={responsive} autoPlay autoPlayInterval="1500"> 
+			<div className="d-flex justify-content-center mx-2" >
+				<AliceCarousel responsive={responsive} autoPlay autoPlayInterval="1500" > 
 				{type.map((place, index) => (    
 						
-						<div key={index} className='me-1'>
+						<div key={index} className=' me-1 d-flex justify-content-center'>
 							
-							<div className="card-body">
-									<h5 className="card-title">{place.name}</h5>
-									<p>{place.vicinity}</p>
+							<div  style={{height: '175px'}}>
+									<h5 className="card-title text-center">{place.name}</h5>
+									<p className="text-center">{place.vicinity}</p>
+									<div className="mt-1 p-2 text-center">
 										<a href={`https://www.google.com/maps/search/?api=1&query=${place.geometry.location.lat},${place.geometry.location.lng}`} target="_blank">
 											<button className="button">
 												See on Google Maps
 											</button>
 										</a>
+									</div>
 								</div>
 						</div>
 				  
